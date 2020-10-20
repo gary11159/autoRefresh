6ec2395063722faba83dc0f45854a8a20b2d108f
Gary.Yang <gary.yang@hy-tech.com.tw>
2020-10-20 15:30:44 +0800

    Updates

---
+++ precache-manifest.fc58792516b3e9d1a1da8450665a39f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d299651ed8ea852587437000d7b2e54",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4890daa265f2e21cec37",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "34ea6a697b0529ecbc16",
+    "url": "/static/js/2.9939aa93.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9939aa93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4890daa265f2e21cec37",
+    "url": "/static/js/main.616b4318.chunk.js"
+  },
+  {
+    "revision": "4d1d30b31be303923679",
+    "url": "/static/js/runtime-main.d683dd63.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
